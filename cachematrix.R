be87d03b4c080ad836596eb30188a056c0bc4f81
MatttyD <mattydolan@gmail.com>
2014-09-21 23:12:57 +1000

    First and Final Commit for Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Assignment 2: Lexical Scoping
+## Cacrhe Calculation of Matrix Inversion
 
-## Write a short comment describing this function
+## Creation of referable  vector based upon Matrix vector input.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+m <- NULL
+set <- function(y) {
+  x <<- y
+  m <<- NULL
+}
+get <- function() x
+setinverse <- function(inverse) m <<- inverse
+getinverse <- function() m
+list(set = set, get = get,
+     setinverse = setinverse,
+     getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## Creation of inversion calculation function with caching checking procedures 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  # Checks for pre-calculated results 
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data,...) #Inversion calculation
+  x$setinverse(m)
+  m
 }
+
+
